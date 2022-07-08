64b4cf746ed5e8c8039a2a27669cf79cc47bfdb4
imexotic <hubert.stoklosa23@gmail.com>
2022-07-08 18:13:09 +0100

    added 'listpolls' command using menus, buttons and pagination

--- exobot/cogs/polls.py
+++ exobot/cogs/polls.py
@@ -6,8 +6,94 @@ sys.path.append(os.path.abspath(os.path.join(dir_path, os.pardir)))
 
 import discord
 import shortuuid
-from discord.ext import commands
+from discord import ui
+from discord.ext import commands, menus
 from managers.database import db, cursor
+
+
+# listpolls - Menu with pagination
+
+class MySource(menus.ListPageSource):
+    async def format_page(self, menu, entries):
+        bot = menu.ctx.bot
+        embed = discord.Embed(
+            title = "Polls Overview",
+            description = f"Page {menu.current_page}/3",
+            color = discord.Colour.random()
+        )
+
+
+        for page in entries:
+            channel = await bot.fetch_channel(page['channel'])
+            message = await channel.fetch_message(page['message'])
+
+            # print(channel, message)
+
+            embed.add_field(
+                name = f"ID: {page['id']}",
+                value = f"[{page['question']}]({message.jump_url})", 
+                inline = False
+            )
+
+        embed.set_footer(text=f"Requested by {menu.ctx.author}")
+        return embed
+
+
+class MyMenuPages(ui.View, menus.MenuPages):
+    def __init__(self, source):
+        super().__init__(timeout=60)
+        self._source = source
+        self.current_page = 0
+        self.ctx = None
+        self.message = None
+
+
+    async def start(self, ctx, *, channel=None, wait=False):
+        # We wont be using wait/channel, you can implement them yourself. This is to match the MenuPages signature.
+        await self._source._prepare_once()
+        self.ctx = ctx
+        self.message = await self.send_initial_message(ctx, ctx.channel)
+
+
+    async def _get_kwargs_from_page(self, page):
+        """This method calls ListPageSource.format_page class"""
+        value = await super()._get_kwargs_from_page(page)
+
+        if 'view' not in value:
+            value.update({'view': self})
+
+        return value
+
+
+    async def interaction_check(self, interaction):
+        """Only allow the author that invoke the command to be able to use the interaction"""
+        return interaction.user == self.ctx.author
+
+
+    # This is extremely similar to Custom MenuPages(I will not explain these)
+    @ui.button(emoji='⏮', style=discord.ButtonStyle.blurple)
+    async def first_page(self, button, interaction):
+        await self.show_page(0)
+
+
+    @ui.button(emoji='⏭', style=discord.ButtonStyle.blurple)
+    async def last_page(self, button, interaction):
+        await self.show_page(self._source.get_max_pages() - 1)
+
+
+    @ui.button(emoji='◀', style=discord.ButtonStyle.blurple)
+    async def before_page(self, button, interaction):
+        await self.show_checked_page(self.current_page - 1)
+
+
+    @ui.button(emoji='▶', style=discord.ButtonStyle.blurple)
+    async def next_page(self, button, interaction):
+        await self.show_checked_page(self.current_page + 1)
+
+
+    @ui.button(emoji='<:stop_check:754948796365930517>', style=discord.ButtonStyle.blurple)
+    async def stop_page(self, button, interaction):
+        self.stop()
 
 
 
@@ -77,6 +163,17 @@ class Polls(commands.Cog):
         #components=[Button(style=ButtonStyle.red, label="60 million years ago", custom_id="button1", disabled = True), Button(style=ButtonStyle.red, label="65 million years ago", custom_id="button2")]
 
         await ctx.send(embed=close_embed)
+
+    @commands.command()
+    async def listpolls(self, ctx):
+        cursor.execute("SELECT * FROM polls")
+        polls = cursor.fetchall()
+
+        formatter = MySource(polls, per_page=3)
+        menu = MyMenuPages(formatter)
+
+        await menu.start(ctx)
+
 
 
     @commands.Cog.listener()
